3955df6271eb7418b0e567c117fd66e08db498e6
Your Name <axel.block@ukbonn.de>
2024-02-20 16:59:53 +0100

    session3 beginn

--- Day2/Session3/src/main.cpp
+++ Day2/Session3/src/main.cpp
@@ -15,3 +15,36 @@
 // [ ] Make a countdown: have the numbers count down to zero.
 // [ ] Restart the countdown every time a button is pressed.
 
+
+#include <Arduino.h>
+
+void setup() {
+  int segmentPins[] = {2,3,6,7,8,9,10,11,12}; // define pins to set as outputs
+  int segmentPins_3[] = {6,7,8,9,12}; // define pins to set as outputs
+  
+  for (int i = 0; i < 9; i++){ 
+    pinMode(segmentPins[i], OUTPUT);
+  }
+
+  digitalWrite(2,HIGH); // set pin 2 as HIGH to turn on 1st digit
+
+  //set all pins to high to clear display
+  for (int i = 2; i < 10; i++){ 
+    digitalWrite(segmentPins[i], HIGH);
+  }
+
+ digitalWrite(6,LOW);
+ digitalWrite(7,LOW);
+ digitalWrite(8,LOW);
+ digitalWrite(9,LOW);
+ digitalWrite(12,LOW);
+
+
+
+}
+
+void loop ()
+{
+
+
+}
